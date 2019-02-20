095c457206cbcbf10c29bd5591379f1d4235c7df
edwardr <lopezredd@gmail.com>
2019-02-19 23:17:52 -0400

    Inject intent variable

--- ios/RNPaypal/RNPaypal.m
+++ ios/RNPaypal/RNPaypal.m
@@ -88,6 +88,7 @@ RCT_REMAP_METHOD(paymentRequest,
     self.payment = [[PayPalPayment alloc] init];
     _payment.amount = params[@"price"];
     _payment.currencyCode = params[@"currency"];
+    _payment.intent = [params[@"intent"] intValue];
     _payment.shortDescription = params[@"description"];
     _config.acceptCreditCards = params[@"acceptCreditCards"];
 }
